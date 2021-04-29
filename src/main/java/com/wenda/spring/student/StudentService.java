c4f5defb912d53ce57b1c01e49ae3f3a7339d5cf
WendaZheng <dylan.d.zheng@gmail.com>
2021-04-29 11:17:59 +0200

    Spring boot server change

---
+++ src/main/java/com/wenda/spring/student/StudentService.java
@@ -0,0 +1,22 @@
+package com.wenda.spring.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Lisa",
+                        "lisa.sven@gmail.com",
+                        LocalDate.of(2000, Month.FEBRUARY,5),
+                        21
+                )
+        );
+    }
+}
